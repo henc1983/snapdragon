3f29c9473479b37b2269b11f304a1422997a392e
Makai Henrik <henc19830328@gmail.com>
2025-10-17 16:49:30 +0200

    Add some theme supports

--- inc/class-snapdragon-setup.php
+++ inc/class-snapdragon-setup.php
@@ -43,6 +43,65 @@ if ( ! class_exists( 'SnapdragonSetup' ) ) :
 			
 			add_action( 'wp_enqueue_scripts', fn()=>$this->enqueue_styles(), 10 );
 
+			add_action( 'after_setup_theme', fn()=>$this->theme_supports() );
+			add_action( 'after_setup_theme', fn()=>$this->load_theme_textdomains() );
+
+		}
+
+
+
+		private function theme_supports() {
+			
+			// Add default posts and comments RSS feed links to head.
+			add_theme_support( 'automatic-feed-links' );
+
+			/*
+			 * Enable support for Post Thumbnails on posts and pages.
+			 *
+			 * @link https://developer.wordpress.org/reference/functions/add_theme_support/#Post_Thumbnails
+			 */
+			add_theme_support( 'post-thumbnails' );
+
+			// Declare support for title theme feature.
+			add_theme_support( 'title-tag' );
+
+			// Declare support for selective refreshing of widgets.
+			add_theme_support( 'customize-selective-refresh-widgets' );
+
+			// Add support for Block Styles.
+			add_theme_support( 'wp-block-styles' );
+
+			// Add support for full and wide align images.
+			add_theme_support( 'align-wide' );
+
+			// Add support for editor styles.
+			add_theme_support( 'editor-styles' );
+
+			// Add support for responsive embedded content.
+			add_theme_support( 'responsive-embeds' );
+
+			/**
+			 * Add support for appearance tools.
+			 *
+			 * @link https://wordpress.org/documentation/wordpress-version/version-6-5/#add-appearance-tools-to-classic-themes
+			 */
+			add_theme_support( 'appearance-tools' );
+
+			
+		}
+
+
+
+		private function load_theme_textdomains() {
+			// Loads wp-content/languages/themes/storefront-it_IT.mo.
+			load_theme_textdomain( 'snapdragon', trailingslashit( WP_LANG_DIR ) . 'themes' );
+
+			// Loads wp-content/themes/child-theme-name/languages/it_IT.mo.
+			load_theme_textdomain( 'snapdragon', get_stylesheet_directory() . '/languages' );
+
+			// Loads wp-content/themes/storefront/languages/it_IT.mo.
+			load_theme_textdomain( 'snapdragon', get_template_directory() . '/languages' );
+
 		}
 
 
